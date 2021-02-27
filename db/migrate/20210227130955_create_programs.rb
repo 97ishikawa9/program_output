53670266cc5204eace0db6f70901e8c0c10be6b7
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2021-02-27 22:19:43 +0900

    programテーブルのマイグレーションファイルの編集

--- db/migrate/20210227130955_create_programs.rb
+++ db/migrate/20210227130955_create_programs.rb
@@ -1,7 +1,10 @@
 class CreatePrograms < ActiveRecord::Migration[6.0]
   def change
     create_table :programs do |t|
-
+      t.string :date, null:false
+      t.string :title, null:false
+      t.text   :fact, null:false
+      t.text   :feeling, null:false
       t.timestamps
     end
   end
