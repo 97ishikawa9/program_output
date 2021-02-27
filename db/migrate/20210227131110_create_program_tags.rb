98044a3bde41fdebf20f0c38c17c97ba4c1b5126
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2021-02-27 22:20:29 +0900

    program_tagテーブルのマイグレーションファイル編集

--- db/migrate/20210227131110_create_program_tags.rb
+++ db/migrate/20210227131110_create_program_tags.rb
@@ -1,7 +1,8 @@
 class CreateProgramTags < ActiveRecord::Migration[6.0]
   def change
     create_table :program_tags do |t|
-
+      t.references :program, foreign_key: true
+      t.references :tag, foreign_key: true
       t.timestamps
     end
   end
