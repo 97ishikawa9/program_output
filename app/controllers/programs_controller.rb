171ee402406449b8140e57da97ec0cc83d23cc1c
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2021-02-28 21:54:38 +0900

    programsコントローラーへの記述

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,2 +1,11 @@
 class ProgramsController < ApplicationController
+
+  def index
+    @programs = Program.all.order(created_at: :desc)
+  end
+
+  def new
+    @program = ProgramsTagRelation.new
+  end
+  
 end
