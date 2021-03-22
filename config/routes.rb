f76862cbd8495510dc8380312b9cf0c3c3b75e2b
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2021-03-22 11:17:20 +0900

    editアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'programs#index'
-  resources :programs, only: [:index, :new, :create, :destroy] do
+  resources :programs, only: [:index, :new, :create, :destroy, :edit] do
     collection do
       get 'search'
     end
