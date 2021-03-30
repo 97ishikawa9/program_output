4e256bffa818b644fcfa7e362715ca9803323356
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2021-03-30 22:26:39 +0900

    ルーティングにshowアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'programs#index'
-  resources :programs, only: [:index, :new, :create] do
+  resources :programs, only: [:index, :new, :create, :show] do
     collection do
       get 'search'
     end
